347c2a903ed64a86e1080d22c355dd6c61399376
Afonin Andrey <andreyafonin96@gmail.com>
2022-12-21 02:01:33 +0400

    init

--- src/ArchiveTrading/index.jsx
+++ src/ArchiveTrading/index.jsx
@@ -7,6 +7,7 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import { format } from "date-fns";
+import styles from './index.module.css'
 
 export function ArchiveTrading(props) {
   return (
@@ -24,7 +25,11 @@ export function ArchiveTrading(props) {
         <TableBody>
           {props.deals.map((deal, index) => (
             <TableRow key={index}>
-              <TableCell >
+              <TableCell
+                className={
+                  deal.type === "Buy" ? styles.actionGreen : styles.actionRed
+                }
+              >
                 {deal.type}
               </TableCell>
               <TableCell>

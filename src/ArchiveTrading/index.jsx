851fee9f2fad9ef6872c20b67288c30d50142392
Afonin Andrey <andreyafonin96@gmail.com>
2022-12-21 02:07:32 +0400

    initfin

--- src/ArchiveTrading/index.jsx
+++ src/ArchiveTrading/index.jsx
@@ -7,7 +7,7 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import { format } from "date-fns";
-import styles from './style.module.css'
+import styles from './index.module.css'
 
 export function ArchiveTrading(props) {
   return (
